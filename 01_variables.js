12ba1c82fd9ec296215e7851ca7039a2f6ebafc7
Rahul Shinde <rahul@Rahuls-MacBook-Air.local>
2026-03-29 15:49:29 +0530

    recheck

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail ="rahul@email.com"
+var accountPassword = "12345"
+accountCity = "Nashik"
+
+//accountId = 2
+
+accountEmail = "rahul1@email.com"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/* 
+do not use var because of issue in block scope and function scope
+
+*/
