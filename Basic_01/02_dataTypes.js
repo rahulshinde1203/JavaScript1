3c0d6ad5b4289019351f6f6e1f2a1efd0db64f79
Rahul Shinde <rahul@Rahuls-MacBook-Air.local>
2026-03-29 15:48:02 +0530

    update

---
+++ Basic_01/02_dataTypes.js
@@ -0,0 +1,24 @@
+// number => 2 to power 53
+// string => "Rahul"
+// boolean => true or false
+// null => empty value
+// undefined => value is not assigned
+// object => array, object literals, functions, dates etc
+// big => 2 to power 100
+//symbol => unique identifier
+//typeof => to check the type of variable
+
+
+
+
+
+
+const accountId = 144553
+let accountEmail ="rahul@gmail.com"
+let isLoggedIn = true
+let userAddress = null
+let userPhone
+let user = {
+    name: "Rahul",
+    age: 30
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JavaScript1
+
+
+/* 
+do not use var because of issue in block scope and function scope
+
+*/
